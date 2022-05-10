f8d260ca17e173029131fc75f7915dd0e0059cec
nishanbhat <rananishanbhat@gmail.com>
2022-05-10 15:02:15 +0545

    login, navigator bar

---
+++ smooth_player_app/lib/api/model/user_model.dart
@@ -0,0 +1,39 @@
+class User {
+  String? id;
+  String? username;
+  String? email;
+  String? password;
+  String? profile_name;
+  String? profile_picture;
+  String? gender;
+  String? birth_date;
+  String? biography;
+  String? verified;
+  String? admin;
+  String? profile_publication;
+  String? followed_artist_publication;
+  String? liked_song_publication;
+  String? liked_album_publication;
+  String? liked_featured_playlist_publication;
+  String? created_playlist_publication;
+
+  User({
+    this.id,
+    this.username,
+    this.email,
+    this.password,
+    this.profile_name,
+    this.profile_picture,
+    this.gender,
+    this.birth_date,
+    this.biography,
+    this.verified,
+    this.admin,
+    this.profile_publication,
+    this.followed_artist_publication,
+    this.liked_album_publication,
+    this.liked_song_publication,
+    this.liked_featured_playlist_publication,
+    this.created_playlist_publication,
+  });
+}
