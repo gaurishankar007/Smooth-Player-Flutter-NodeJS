77dc258f4a8f50e4d662b88ee4422d70713340f7
Rupendra-coder <magarrupendra@gmail.com>
2022-05-12 13:56:19 +0545

    Further Updating

--- smooth_player_api/setting/songSetting.js
+++ smooth_player_api/setting/songSetting.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storageNavigation = multer.diskStorage({
     destination: function(req, file, cb) {
-        if(file.mimetype == "image/png" || file.mimetype == "image/jpeg") {            
+        if(file.mimetype == "image/png" || file.mimetype == "image/jpeg" || file.mimetype=="application/octet-stream" ) {            
             cb(null, "../smooth_player_api/upload/image/song");
         } else if(file.mimetype == "audio/mpeg" || file.mimetype=="audio/mp4") {
             cb(null, "../smooth_player_api/upload/music");
