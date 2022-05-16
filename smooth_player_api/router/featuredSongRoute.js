c3ce84f6b0802daa4cb00c7f263b5e820b039af8
Sanjaya Thapa <57608136+Anil-account@users.noreply.github.com>
2022-05-16 16:34:43 +0545

    backend of song added in featured playlist

--- smooth_player_api/router/featuredSongRoute.js
+++ smooth_player_api/router/featuredSongRoute.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const router = new express.Router();
 const auth = require('../authentication/auth');
 const featuredSong = require('../model/featureSongModel');
@@ -8,5 +9,17 @@ router.delete("/delete/featuredPlaylistSong", auth.verifyUser, async (req, res)=
         res.send({resM: "Featured Playlist Song has been deleted."});
     });   
 });
+
+
+router.post("/upload/featuredSong", auth.verifyAdmin, async(req, res)=> {
+    const newFeaturedSong = new playlist({
+        featuredPlaylist: mongoose.Types.ObjectId(req.body.featuredPlaylistId),
+        song: mongoose.Types.ObjectId(req.body.songId)
+    })
+
+    newFeaturedSong.save().then(()=> {
+        res.status(201).send({resM: "'" + req.body.title + "'" + " featured song added."});
+    }); 
+})
 
 module.exports = router;
