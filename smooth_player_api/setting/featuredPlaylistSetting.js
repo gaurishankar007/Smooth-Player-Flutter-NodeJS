9acedcc1d95d2d4c2dcf093762d668f6f31cbed7
Shikhar Manandhar <shikharaa26@gmail.com>
2022-05-16 08:28:13 +0545

    testing of featured playlist

---
+++ smooth_player_api/setting/featuredPlaylistSetting.js
@@ -0,0 +1,27 @@
+const multer = require("multer");
+
+const storageNavigation = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "../smooth_player_api/upload/image/featuredPlaylist");
+    },
+    filename: function(req, file, cb) {
+        cb(null, Date.now()+"_"+file.originalname);
+    }
+});
+
+
+const filter = function(req, file, cb) {
+    if(file.mimetype == "image/png" || file.mimetype=="image/jpeg" || file.mimetype=="application/octet-stream" ) {
+        cb(null, true);
+    }
+    else {
+        cb(null, false);
+    }
+}
+
+const upload = multer({
+    storage: storageNavigation,
+    fileFilter: filter
+});
+
+module.exports = upload;
