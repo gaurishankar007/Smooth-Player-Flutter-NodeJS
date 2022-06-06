e548cc7caba3d845c52f3a50e7f2caf8c0781bc5
gaurishankar007 <57608187+gaurishankar007@users.noreply.github.com>
2022-06-06 07:17:13 +0545

    follow check

--- smooth_player_api/router/followRoute.js
+++ smooth_player_api/router/followRoute.js
@@ -5,21 +5,49 @@ const auth = require("../authentication/auth");
 const follow = require("../model/followModel");
 
 router.post("/follow/artist", auth.verifyUser, (req, res) => {
-  const newFollow = new follow({
-    user: req.userInfo._id,
-    artist: mongoose.Types.ObjectId(req.body.artistId),
-  });
+  const artistId = req.body.artistId;
+  const artistName = req.body.artistName;
+  follow
+    .findOne({
+      user: req.userInfo._id,
+      artist: artistId,
+    })
+    .then((followData) => {
+      if (followData === null) {
+        const newFollow = new follow({
+          user: req.userInfo._id,
+          artist: mongoose.Types.ObjectId(req.body.artistId),
+        });
 
-  newFollow.save().then(() => {
-    res.status(201).send({ resM: "You followed " + req.body.artistName });
-  });
+        newFollow.save().then(() => {
+          res.status(201).send({ resM: "You followed " + artistName });
+        });
+      } else {
+        follow
+          .findOneAndDelete({
+            user: req.userInfo._id,
+            artist: req.body.artistId,
+          })
+          .then(() => {
+            res.send({ res: "You unfollowed " + artistName });
+          });
+      }
+    });
 });
 
-router.delete("/unFollow/artist", auth.verifyUser, (req, res) => {
+router.post("/follow/checkArtist", auth.verifyUser, (req, res) => {
+  const artistId = req.body.artistId;
   follow
-    .findOneAndDelete({ user: req.userInfo._id, artist: req.body.artistId })
-    .then(() => {
-      res.send({ res: "You unfollowed " + req.body.artistName });
+    .findOne({
+      user: req.userInfo._id,
+      artist: artistId,
+    })
+    .then((followData) => {
+      if (followData === null) {
+        res.send(false);
+      } else {
+        res.send(true);
+      }
     });
 });
 
