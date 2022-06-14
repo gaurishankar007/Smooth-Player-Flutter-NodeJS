555fa47b33970614bc239309be75b354c1a47cb8
Sanjaya Thapa <57608136+Anil-account@users.noreply.github.com>
2022-06-14 10:03:04 +0545

    content added

--- smooth_player_app/lib/screen/authentication/forget_password.dart
+++ smooth_player_app/lib/screen/authentication/forget_password.dart
@@ -8,12 +8,34 @@ class ForgetPassword extends StatefulWidget {
 }
 
 class _ForgetPasswordState extends State<ForgetPassword> {
+  final _formKey = GlobalKey<FormState>();
+  String email = "", password = "", confirmPassword = "";
+
+
   @override
   Widget build(BuildContext context) {
+    final sWidth = MediaQuery.of(context).size.width;
+    final sHeight = MediaQuery.of(context).size.height;
     return Scaffold(
+            appBar: AppBar(
+        title: Text("SmoothPlayer"),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
       body: SafeArea(
         child: Center(
           child: Text('Forget Password'),
+
         ),
       ),
       
