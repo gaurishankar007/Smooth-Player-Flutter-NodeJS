90dd1a2c246319f518ddfa86f114e8ae90dd7529
Sanjaya Thapa <57608136+Anil-account@users.noreply.github.com>
2022-06-21 09:46:15 +0545

    token dart

---
+++ smooth_player_app/lib/screen/authentication/enter_token.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EnterToken extends StatefulWidget {
+  const EnterToken({ Key? key }) : super(key: key);
+
+  @override
+  State<EnterToken> createState() => _EnterTokenState();
+}
+
+class _EnterTokenState extends State<EnterToken> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
