5b8688a3377a1230a3b06b0d434f2c298fa03228
Rupendra-coder <magarrupendra@gmail.com>
2022-06-23 10:43:28 +0545

    URL Update

--- smooth_player_app/lib/api/urls.dart
+++ smooth_player_app/lib/api/urls.dart
@@ -1,8 +1,8 @@
 class ApiUrls {
-  static const routeUrl = "http://192.168.137.38:8080/";
-  static const musicUrl = "http://192.168.137.38:8080/music/";
-  static const coverImageUrl = "http://192.168.137.38:8080/image/albumSong/";
-  static const profileUrl = "http://192.168.137.38:8080/image/user/";
-  static const featuredPlaylistUrl = "http://192.168.137.38:8080/image/featuredPlaylist/";
-  static const genreUrl = "http://192.168.137.38:8080/image/genre/";
+  static const routeUrl = "http://10.0.2.2:8080/";
+  static const musicUrl = "http://10.0.2.2:8080/music/";
+  static const coverImageUrl = "http://10.0.2.2:8080/image/albumSong/";
+  static const profileUrl = "http://10.0.2.2:8080/image/user/";
+  static const featuredPlaylistUrl = "http://10.0.2.2:8080/image/featuredPlaylist/";
+  static const genreUrl = "http://10.0.2.2:8080/image/genre/";
 }
